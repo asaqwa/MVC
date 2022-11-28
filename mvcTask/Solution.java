3de63f11276cfd96325638615cb568b3d5717799
Andrey <bursov@web.de>
2022-11-28 11:50:46 +0100

    task three JavaRush provides Solution.java

---
+++ mvcTask/Solution.java
@@ -0,0 +1,18 @@
+import controller.Controller;
+import model.FakeModel;
+import model.Model;
+import view.UsersView;
+
+public class Solution {
+    public static void main(String[] args) {
+        Model model = new FakeModel();
+        UsersView usersView = new UsersView();
+        Controller controller = new Controller();
+
+        usersView.setController(controller);
+        controller.setModel(model);
+        controller.setUsersView(usersView);
+
+        usersView.fireEventShowAllUsers();
+    }
+}
