c92efbc7b660f6f6f2eaeaf09838bee4695231b9
Andrey <bursov@web.de>
2022-11-29 13:25:12 +0100

    Task four completed

--- example/mvcTask/Solution.java
+++ example/mvcTask/Solution.java
@@ -2,12 +2,13 @@ package mvcTask;
 
 import mvcTask.controller.Controller;
 import mvcTask.model.FakeModel;
+import mvcTask.model.MainModel;
 import mvcTask.model.Model;
 import mvcTask.view.UsersView;
 
 public class Solution {
     public static void main(String[] args) {
-        Model model = new FakeModel();
+        Model model = new MainModel();
         UsersView usersView = new UsersView();
         Controller controller = new Controller();
 
---
+++ example/mvcTask/model/MainModel.java
@@ -0,0 +1,19 @@
+package mvcTask.model;
+
+import mvcTask.model.service.UserService;
+import mvcTask.model.service.UserServiceImpl;
+
+public class MainModel implements Model {
+    private ModelData modelData = new ModelData();
+    private UserService userService = new UserServiceImpl();
+
+    @Override
+    public void loadUsers() {
+        modelData.setUsers(userService.getUsersBetweenLevels(1, 100));
+    }
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+}
